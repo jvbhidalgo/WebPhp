09efc466ea4771fe953a416847cefe7cc7c9908f
Joao Victor Hidalgo <jvbhidalgo97@gmailcom>
2020-08-29 17:27:00 -0300

    sql usa ` em vez de '

--- arquivosSQL/TabelaCampo.sql
+++ arquivosSQL/TabelaCampo.sql
@@ -1,42 +1,42 @@
--- Banco de dados: 'primat'
+-- Banco de dados: `primat`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela 'usucad'
+-- Estrutura da tabela `usucad`
 --
 
-CREATE TABLE 'usucad' (
-  'USUID' int(5) NOT NULL,
-  'USULOGIN' varchar(24) NOT NULL,
-  'USUNOME' varchar(255) NOT NULL,
-  'USUMAIL' varchar(255) NOT NULL,
-  'USUFONE' varchar(20) NOT NULL,
-  'USUCEP' varchar(8) NOT NULL,
-  'USURUA' varchar(255) NOT NULL,
-  'USUBAIRRO' varchar(255) NOT NULL,
-  'USUCIDA' varchar(255) NOT NULL,
-  'USUESTA' varchar(2) NOT NULL,
-  'USUENDN' varchar(10) NOT NULL,
-  'USUSENHA' varchar(20) NOT NULL
+CREATE TABLE `usucad` (
+  `USUID` int(5) NOT NULL,
+  `USULOGIN` varchar(24) NOT NULL,
+  `USUNOME` varchar(255) NOT NULL,
+  `USUMAIL` varchar(255) NOT NULL,
+  `USUFONE` varchar(20) NOT NULL,
+  `USUCEP` varchar(8) NOT NULL,
+  `USURUA` varchar(255) NOT NULL,
+  `USUBAIRRO` varchar(255) NOT NULL,
+  `USUCIDA` varchar(255) NOT NULL,
+  `USUESTA` varchar(2) NOT NULL,
+  `USUENDN` varchar(10) NOT NULL,
+  `USUSENHA` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tabela 'usucad'
+-- Índices para tabela `usucad`
 --
-ALTER TABLE 'usucad'
-  ADD PRIMARY KEY ('USUID');
+ALTER TABLE `usucad`
+  ADD PRIMARY KEY (`USUID`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela 'usucad'
+-- AUTO_INCREMENT de tabela `usucad`
 --
-ALTER TABLE 'usucad'
-  MODIFY 'USUID' int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+ALTER TABLE `usucad`
+  MODIFY `USUID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
