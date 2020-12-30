5fbf61bd2fba12e903b9424d9101c515ecff7078
João Victor Barbosa Hidalgo <54051455+jvbhidalgo@users.noreply.github.com>
2020-12-30 13:27:59 -0300

    Update connection.php

--- classes/connection.php
+++ classes/connection.php
@@ -2,7 +2,7 @@
 
   $database_username = "ba97608f39dcac";
   $database_password = "010cc935";
-  $database_info = "mysql:host=ip-10-0-104-71;dbname=heroku_ff43c68a0daab53";
+  $database_info = "mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_ff43c68a0daab53";
   try
   {
       $con = new PDO($database_info, $database_username, $database_password);
